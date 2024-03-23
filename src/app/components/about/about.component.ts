01883c7f10997f1d5e8545a2e18ce8b8bf5e39da
burhanHere <burhanburewala@gmail.com>
2024-03-23 13:03:49 +0500

    completed about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
-
+  linkAddress: string;
+  constructor() {
+    this.linkAddress = '/home';
+  }
 }
